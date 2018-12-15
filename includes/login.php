6108d7f392cdf47951ec1cebd9358027f6e4ced7
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2018-12-15 17:23:41 +0100

    link do panelu

--- includes/login.php
+++ includes/login.php
@@ -4,6 +4,10 @@
                     <?php if (isset($_SESSION['user']['username'])): ?>
                         <li class="element">Witaj <?php echo $_SESSION['user']['username'] ?>!</li>
                         <li class="separator"><img src="static/media/menu_oddzielacz2.gif" alt="::"></li>
+                        <?php if ($_SESSION['user']['role'] == 'Admin'): ?>
+                            <li class="element"><a href="panel.php">Panel administracyjny</a></li>
+                            <li class="separator"><img src="static/media/menu_oddzielacz2.gif" alt="::"></li>
+                        <?php endif ?>
                         <li class="element"><a href="logout.php">Wyloguj</a></li>
                     <?php else: ?>
                         <li class="element"><a href="register.php">Zarejestruj się!</a></li>
