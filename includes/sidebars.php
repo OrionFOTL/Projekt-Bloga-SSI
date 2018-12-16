0d9f8a24bec3a0908dacced495b9770821b17f02
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2018-12-16 21:28:44 +0100

    najnowsze artykuły po bokach

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -1,9 +1,10 @@
+<?php $posts = getPublishedPosts(); ?>
 <div id="left">
 			<h1>Najnowsze artykuły</h1>
 			<ul>
-				<li><a href="http://www.techpowerup.com/208653/corsair-launches-the-hx1200i-power-supply.html">Corsair przygotowuje 1200W zasilacz z serii HXi</a></li>
-				<li><a href="http://www.techpowerup.com/live/CES_2015/Rosewill">Rosewill Quark 1000W spostrzeżony na targach CES 2015</a></li>
-				<li><a href="http://www.kitguru.net/components/power-supplies/zardon/fsp-aurum-pt-1000w-psu-review/">Fortron Aurum PT przetestowany</a></li>
+				<?php foreach ($posts as $post): ?>
+				<li><a href="post.php?post-slug=<?php echo $post['slug']; ?>"><?php echo $post['title']; ?></a></li>
+				<?php endforeach ?>
 			</ul>
 		</div>	    
 		<div id="right">
